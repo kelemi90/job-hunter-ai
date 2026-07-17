65409f7857c6bf978ddffb022dde7afb5f780920
kelmi <138400315+kelemi90@users.noreply.github.com>
2026-07-17 17:15:16 +0300

    update

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { configuration } from './config';
+import { PrismaModule } from './prisma/prisma.module';
+import { ScraperModule } from './scraper/scraper.module';
+import { MatchingModule } from './matching/matching.module';
+import { UserModule } from './user/user.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
   imports: [
@@ -10,6 +16,12 @@ import { configuration } from './config';
       load: [configuration],
       envFilePath: '.env',
     }),
+    PrismaModule,
+    ScraperModule,
+    MatchingModule,
+    UserModule,
+    NotificationsModule,
+    SchedulerModule,
   ],
 })
 export class AppModule {}
